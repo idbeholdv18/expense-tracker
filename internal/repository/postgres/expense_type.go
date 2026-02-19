a4247397c849f2296bccab3db381275f121770ea
idbeholdv18 <alimsadullaev18@gmail.com>
2026-02-19 13:31:05 +0300

    feat: get expense type and delete by id repo methods

--- internal/repository/postgres/expense_type.go
+++ internal/repository/postgres/expense_type.go
@@ -3,6 +3,7 @@ package postgres
 import (
 	"context"
 	"database/sql"
+	"errors"
 	"github/idbeholdv18/expense-tracker/internal/repository"
 )
 
@@ -18,4 +19,51 @@ func (r *PostgresExpenseTypeRepository) Create(ctx context.Context, t *repositor
 	`
 
 	return r.db.QueryRowContext(ctx, query, t.UserID, t.Name).Scan(&t.ID, &t.CreatedAt)
+}
+
+func (r *PostgresExpenseTypeRepository) DeleteByID(ctx context.Context, userID int, expenseTypeID int) error {
+	query := `
+		DELETE FROM expenses.expense_types
+		WHERE user_id=$1 AND id=$2;
+	`
+
+	res, err := r.db.ExecContext(ctx, query, userID, expenseTypeID)
+	if err != nil {
+		return err
+	}
+
+	rows, err := res.RowsAffected()
+	if err != nil {
+		return err
+	}
+
+	if rows == 0 {
+		return errors.New("expense type not found or not owned by user")
+	}
+
+	return nil
+}
+
+func (r *PostgresExpenseTypeRepository) GetById(ctx context.Context, userID int, expenseTypeID int) (*repository.ExpenseType, error) {
+	query := `
+		SELECT id, user_id, name, created_at
+		FROM expenses.expense_types
+		WHERE user_id=$1 AND id=$2;
+	`
+
+	expenseType := &repository.ExpenseType{}
+
+	err := r.db.
+		QueryRowContext(ctx, query, userID, expenseTypeID).
+		Scan(&expenseType.ID, &expenseType.UserID, &expenseType.Name, &expenseType.CreatedAt)
+
+	if errors.Is(err, sql.ErrNoRows) {
+		return nil, nil
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	return expenseType, nil
 }
